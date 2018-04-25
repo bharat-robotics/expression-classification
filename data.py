c8e0dca42b07f9dce28e9512a296254199ecc941
bharat-robotics <robotics.bharat@gmail.com>
2018-04-25 11:36:56 -0400

    Use 5 images from a emotion sequence

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import os
 import cv2
 import numpy as np
 import keras
-
+from random import shuffle
 class LoadData():
 
     def __init__(self,emotion_folder,pic_folder):
@@ -11,7 +11,7 @@ class LoadData():
         self.pic_folder = pic_folder
 
 
-    def read_data(self):
+    def read_data(self,train_percentage):
         all_emotions = []
         for root, dirs, files in os.walk(self.emotion_folder):
             for name in files:
@@ -19,36 +19,68 @@ class LoadData():
 
         total_files = len(all_emotions)
         input_files = [None] * total_files
-        labels = [None] * total_files
         for index,emotions in enumerate(all_emotions):
-            folder = emotions[1].split('_');
+            folder = emotions[1].split('_')
             pic_path = os.path.join(self.pic_folder,folder[0],folder[1])
             pic_name = folder[0] + '_' + folder[1] + '_' + folder[2] + '.png'
             picture = os.path.join(pic_path,pic_name)
-            input_files[index] = picture
             with open(emotions[0]) as f:
                 first_line = f.readline()
-            labels[index] = int(float(first_line))
+            input_files[index] = [picture,int(float(first_line))]
 
-        return input_files,labels
+        shuffle(input_files)
+        separation = int(train_percentage*len(input_files))
+        image_train =  input_files[:separation]
+        image_test = input_files[separation:]
+        shuffle(image_test)
 
-    def preprocess(self,images,labels,img_rows,img_cols,train_percentage,num_classes):
-        x = np.zeros((len(images),img_rows,img_cols))
-        for index,image_file in enumerate(images):
+        datas  = []
+        for index,data in enumerate(image_train):
+            file = data[0]
+            splits = file.split('_')
+            pic_seq = int(splits[2].split('.')[0])
+            for i in range(pic_seq,pic_seq-5,-1):
+                if(i < 10):
+                    pic_string = '0000000' + str(i)
+                else:
+                    pic_string = '000000' + str(i)
+                pic_name = splits[0] + '_' + splits[1] + '_' + pic_string + '.png'
+                datas.append([pic_name,data[1]])
+
+        shuffle(datas)
+
+        training_images = [None] * len(datas)
+        training_labels = [None] * len(datas)
+        testing_images = [None] * len(image_test)
+        testing_labels = [None] * len(image_test)
+
+        for index,image_file in enumerate(datas):
+            training_images[index] = image_file[0]
+            training_labels[index] = image_file[1]
+
+        for index,image_file in enumerate(image_test):
+            testing_images[index] = image_file[0]
+            testing_labels[index] = image_file[1]
+
+        return training_images,training_labels,testing_images,testing_labels
+
+    def preprocess(self,train_images,train_labels,test_images,test_labels,img_rows,img_cols,train_percentage,num_classes):
+        x_train = np.zeros((len(train_images),img_rows,img_cols))
+        y_train = np.zeros(len(train_labels))
+        x_test = np.zeros((len(test_images), img_rows, img_cols))
+        y_test = np.zeros(len(test_images))
+
+        for index,image_file in enumerate(train_images):
             img = cv2.imread(image_file,cv2.IMREAD_GRAYSCALE)
             res = cv2.resize(img,(img_cols,img_rows),interpolation= cv2.INTER_CUBIC)
-            x[index] = res
+            x_train[index] = res
+            y_train[index] = train_labels[index]
 
-        indices = np.random.permutation(x.shape[0])
-        separation = int(train_percentage*x.shape[0])
-        training_idx, test_idx = indices[:separation], indices[separation:]
-        x_train, x_test = x[training_idx, :], x[test_idx, :]
-
-        labels = np.array(labels)
-        y_train,y_test = labels[training_idx], labels[test_idx]
-
-        x_train = x_train.reshape(x_train.shape[0],img_rows,img_cols,1)
-        x_test = x_test.reshape(x_test.shape[0],img_rows,img_cols,1)
+        for index, image_file in enumerate(test_images):
+            img = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
+            res = cv2.resize(img, (img_cols, img_rows), interpolation=cv2.INTER_CUBIC)
+            x_test[index] = res
+            y_test[index] = test_labels[index]
 
         x_train = x_train.astype('float32')
         x_test = x_test.astype('float32')
@@ -66,9 +98,20 @@ if __name__== "__main__":
 
     train_ratio = 0.8
     num_of_classes = 8
-    img_rows = 480
-    img_cols = 640
+    img_rows = 224
+    img_cols = 224
 
     loadData = LoadData(emotion_folder,pic_folder)
-    image_names,labels = loadData.read_data()
-    loadData.preprocess(image_names,labels,img_rows,img_cols,train_ratio,num_of_classes)
+
+    train_images,train_labels,test_images,test_labels = loadData.read_data(train_ratio)
+
+
+    x_train,x_test,y_train,y_test = loadData.preprocess(train_images,train_labels,test_images,test_labels,img_rows,img_cols,train_ratio,num_of_classes)
+
+    x_test *= 255.0
+    x_test = x_test.astype('uint8')
+    for t in range(x_test.shape[0]):
+        img = x_test[t,:]
+        cv2.imshow('img',img)
+        cv2.waitKey(0)
+        print(y_test[t])
